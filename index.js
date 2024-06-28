6bf1c59a78b664ed4efe633bd418889302f81663
Asmaa01-hub <husseinasmahan89@gmail.com>
2024-06-28 11:19:02 +0300

    completed

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+
   // Write your code here!
+
+  if(ride <= 400(feet)){
+   return 'free sample';
+
+
+  
+    console.log("free sample")
+
+
+    }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  // Write your code here
+  if (tip === 'generous') {
+    return 'Thank you so much.';
+} else if (tip === 'not as generous') {
+    return 'Thank you.';
+} else {
+    return 'Bye.';
+
+}
 }
